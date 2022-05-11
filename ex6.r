b19832e7c405439755520f5541658ab45697bd2a
Gonçalo Nunes <73646319+Goncalo-Palma-Nunes@users.noreply.github.com>
2022-05-11 18:01:22 -0400

    Update ex6.r

--- ex6.r
+++ ex6.r
@@ -1,7 +1,11 @@
+install.packages("tidyverse")
+library(ggplot2)
 seed <- 1637
 num_amostras <- 500
 lim_inf <- 3
 lim_sup <- 7
+valorEsperado <- (lim_inf + lim_sup) / 2
+variancia <- (lim_sup - lim_inf)^2 / 12
 
 histograma_n <- function(n) {
   vetor_medias <- c()
@@ -12,9 +16,26 @@ histograma_n <- function(n) {
     vetor_medias <- append(vetor_medias, mean(amostra))
     i <- i + 1
   }
-  return(vetor_medias)
+  return(data.frame(medias=vetor_medias))
 }
 
-hist(histograma_n(5), xlab = 'Médias das amostras', main='Histograma para n = 5', col = 'aquamarine')
-hist(histograma_n(29), xlab = 'Médias das amostras', main='Histograma para n = 29', col = 'aquamarine')
-hist(histograma_n(68), xlab = 'Médias das amostras', main='Histograma para n = 68', col = 'aquamarine')
+densidade_n <- function(n) {
+  sequencia <- seq(lim_inf, lim_sup, length.out = 200)
+  return(dnorm(sequencia, valorEsperado, variancia / n))
+}
+
+desenha <- function(n) {
+  df <- histograma_n(n)
+  ggplot(data=df) +
+    geom_histogram(aes(x=medias, colour='aquamarine')) +
+    geom_line(aes(x=densidade_n(n)))
+  print('aaaa')
+}
+
+desenha(5)
+#hist(histograma_n(5), xlab = 'Medias das amostras', main='Histograma para n = 5', col = 'aquamarine')
+hist(histograma_n(5), xlab = 'Medias das amostras', main='Histograma para n = 5', col = 'aquamarine')
+plot(densidade_n(5), colour='red')
+points(densidade_n(5))
+hist(histograma_n(29), xlab = 'Medias das amostras', main='Histograma para n = 29', col = 'aquamarine')
+hist(histograma_n(68), xlab = 'Medias das amostras', main='Histograma para n = 68', col = 'aquamarine')
