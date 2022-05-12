9a0deda600af97068d0e82a9b726abdd393df881
Gonçalo Nunes <73646319+Goncalo-Palma-Nunes@users.noreply.github.com>
2022-05-12 18:08:27 +0100

    Update ex8.r

--- ex8.r
+++ ex8.r
@@ -1,3 +1,5 @@
+library('EnvStats')
+
 seed = 194
 m = 500
 n = 609
@@ -6,4 +8,16 @@ valorEsperado = 1 / lambda
 set.seed(seed)
 confianca = 0.99
 
-#nível de confiança   1 - alpha = 0.99
+set.seed(seed)
+i = 0
+amplitudes <- c()
+while (i < m) {
+  amostra <- rexp(n, lambda)
+  aaa <- eexp(amostra, ci=TRUE, conf = confianca)
+
+  lim_inf <- unname((aaa$interval$limits)[1])
+  lim_sup <- unname((aaa$interval$limits)[2])
+  amplitudes <- append(amplitudes, (lim_sup - lim_inf))
+  i = i + 1
+}
+mean(amplitudes)
